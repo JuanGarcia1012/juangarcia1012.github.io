32da700db60110b574287a86cbc112dcb74a0ca8
juan garcia <jvg28@cornell.edu>
2020-07-23 21:02:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	});
 	
 	var typed = new Typed(".typed", {
-		strings: ["Cornell Undergraduate.", "Operations Research.", "Information Engineering.", "Data Analyst.", "Basketball Fanatic."],
+		strings: ["Product Developer.", "Cornell Graduate.", "Operations Research.", "Information Engineering.", "Data Analyst.", "Basketball Fanatic."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
