576d8dd0109c5b42b104bce1d5de6988358b628f
juan garcia <jvg28@cornell.edu>
2020-07-23 21:11:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 	
 	var typed = new Typed(".typed", {
 		strings: ["Product Developer.", "Cornell Graduate.", "Operations Research.", "Information Engineering.", "Data Scientist.", "Basketball Fanatic."],
-		typeSpeed: 61,
+		typeSpeed: 50,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
